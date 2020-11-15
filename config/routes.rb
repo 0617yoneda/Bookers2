d7fdab2fda9d4f4f6d2fbd1115de84a6a2489227
EC2 Default User <ec2-user@ip-172-31-65-140.ec2.internal>
2020-11-15 10:35:06 +0000

    [add]全カラム追加済み

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+ devise_for :users
  root to: "homes#top"
+ get "about" => "homes#about"
+ resources :books
+ resources :users, only: [:show, :edit, :update]
 end
