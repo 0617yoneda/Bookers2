ff9891cbbc788f50d45101e188cfb966502feb07
EC2 Default User <ec2-user@ip-172-31-65-140.ec2.internal>
2020-11-16 07:29:20 +0000

    [add]部分テンプレート完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,12 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+  
   def create
     book = Book.new(book_params)
     book.user_id = current_user.id
@@ -10,24 +16,22 @@ class BooksController < ApplicationController
     redirect_to book_path(book)
   end
 
-  def index
-    @books = Book.all
-  end
-  
   def show
     @book = Book.find(params[:id])
+    @book = Book.new
+    @user = User.find(params[:id])
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-    
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
