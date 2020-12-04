33fe8f484ffe06b8bed3ac39fa2272cd06a3ef97
EC2 Default User <ec2-user@ip-172-31-65-140.ec2.internal>
2020-12-04 06:52:42 +0000

    [add]フォロー、フォロワーカウント機能実直前装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,27 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy#ユーザーとフォロー人結ぶ　
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy#ユーザーとフォローされる人結ぶ
+  has_many :following_user, through: :follower, source: :followed# 自分がフォローしている人
+  has_many :follower_user, through: :followed, source: :follower# 自分をフォローしている人
+  
+  #フォロー
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+  
+  #フォロー解除
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  
+  #フォローしてればtrue返し
+  def following?(user)
+  following_user.include?(user)
+  end
+  
+  
   attachment :profile_image
   
   validates :name, length: { in: 2..20 }, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
  devise_for :users
  root "homes#home"
  get "/home/about" => "homes#about"
+ post 'follow/:id' => 'relationships#follow', as: 'follow' 
+ post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' 
  resources :books
  resources :users, only: [:index, :show, :edit, :update]
 end
